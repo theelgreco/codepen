f619a06cd4d799c098d349422bb50a0cd8e680d4
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-22 21:02:50 +0100

    add Coordinates component to display mouse position

---
+++ src/client/pages/spraypaint/components/Coordinates.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useMouse } from "@vueuse/core";
+
+const { x, y } = useMouse();
+</script>
+
+<template>
+    <div class="flex flex-col p-5 border w-fit border-neutral-800 select-none fixed bottom-10 right-10 shadow-xl">
+        <span><small class="font-thin">x: </small>{{ x }}</span>
+        <span><small class="font-thin">y: </small>{{ y }}</span>
+    </div>
+</template>
