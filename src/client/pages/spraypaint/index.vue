debf292146ddea876eb9db015a7684df34454978
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-22 21:02:55 +0100

    add useMouseWheel composable for mouse wheel events; update index.html and index.vue for new functionality; refactor style.css to use @layer

--- src/client/pages/spraypaint/index.vue
+++ src/client/pages/spraypaint/index.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
 import { useMouse } from "@vueuse/core";
-import { onMounted, ref } from "vue";
+import Coordinates from "./components/Coordinates.vue";
+import { computed, ref, watch } from "vue";
+import { useMouseWheel } from "./composables";
+import { clamp } from "lodash";
 
-const { x, y } = useMouse();
+const { deltaY } = useMouseWheel();
+const { x: mouseX, y: mouseY } = useMouse();
+
+const radius = ref(35);
+
+const svgProps = computed(() => {
+    const coords = radius.value + 15;
+    const size = coords * 2;
+    const x = mouseX.value - size / 2;
+    const y = mouseY.value - size / 2;
+
+    return {
+        width: size,
+        height: size,
+        cx: coords,
+        cy: coords,
+        r: radius.value,
+        x,
+        y,
+    };
+});
+
+watch(deltaY, (newValue) => {
+    radius.value = clamp(0, radius.value + newValue, 300);
+});
 </script>
 
 <template>
-    <main>
-        <div class="p-5">
-            <h1>
-                <span><small class="font-thin">x: </small></span>
-                {{ x }}
-            </h1>
-            <h1>
-                <span><small class="font-thin">y: </small></span>
-                {{ y }}
-            </h1>
-        </div>
+    <main class="p-10">
+        <svg class="fixed" :style="{ top: svgProps.y, left: svgProps.x }" ref="svg" :width="svgProps.width" :height="svgProps.height">
+            <circle :cx="svgProps.cx" :cy="svgProps.cy" :r="svgProps.r" stroke="white" fill="transparent"></circle>
+        </svg>
     </main>
+    <Coordinates />
 </template>
