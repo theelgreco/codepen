ba839ef85df39d70c8708f4ee2825de49f03f97f
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-22 19:34:19 +0100

    add Vue and VueUse dependencies; update Vite config to include Vue plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { resolve } from "path";
 import { globbySync } from "globby";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 
 const htmlEntries = globbySync("./src/client/**/*.html").reduce((acc, file) => {
     const name = file.replace("./src/client/pages/", "").replace("/index.html", "").replace(".html", "");
+    // @ts-expect-error
     acc[name || "index"] = resolve(__dirname, file);
     return acc;
 }, {});
 
 export default defineConfig({
     root: "./src/client/pages",
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), vue()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src/client", import.meta.url)), // Alias for src folder
