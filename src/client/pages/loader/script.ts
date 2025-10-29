ff759ebb1dba3185b24a7d49001a4b1be8ce8167
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-29 20:43:55 +0000

    feat: implement loader animation with dynamic hand elements

--- src/client/pages/loader/script.ts
+++ src/client/pages/loader/script.ts
@@ -1,1 +1,11 @@
-console.log('Welcome to loader')
+const numHands = 10;
+const animationDuration = 600;
+const loader = document.getElementById("loader") as HTMLDivElement;
+
+for (let i = 0; i < numHands; i++) {
+    const hand = document.createElement("span");
+    hand.style.transform = `rotate(${(360 / numHands) * i}deg)`;
+    hand.style.animationDuration = `${animationDuration}ms`;
+    hand.style.animationDelay = `${(animationDuration / numHands) * i}ms`;
+    loader.appendChild(hand);
+}
