7d62ba3ac4d316fa955af440a537afaff6ef6c65
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-31 21:53:53 +0000

    fix: refactor loader initialization to support multiple loaders with dynamic properties

--- src/client/pages/loader/script.ts
+++ src/client/pages/loader/script.ts
@@ -1,11 +1,14 @@
-const numHands = 20;
-const animationDuration = 600;
-const loader = document.getElementById("loader") as HTMLDivElement;
+const loaders = document.querySelectorAll(".loader");
 
-for (let i = 0; i < numHands; i++) {
-    const hand = document.createElement("span");
-    hand.style.transform = `rotate(${(360 / numHands) * i}deg)`;
-    hand.style.animationDuration = `${animationDuration}ms`;
-    hand.style.animationDelay = `${(animationDuration / numHands) * i}ms`;
-    loader.appendChild(hand);
-}
+loaders.forEach((loader) => {
+    const numHands = parseInt((loader as HTMLDivElement).dataset.numHands || "") || 20;
+    const animationDuration = parseInt((loader as HTMLDivElement).dataset.animationDuration || "") || 600;
+
+    for (let i = 0; i < numHands; i++) {
+        const hand = document.createElement("span");
+        hand.style.transform = `rotate(${(360 / numHands) * i}deg)`;
+        hand.style.animationDuration = `${animationDuration}ms`;
+        hand.style.animationDelay = `${(animationDuration / numHands) * i}ms`;
+        loader.appendChild(hand);
+    }
+});
