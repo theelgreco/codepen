b93b3ccceecc4bb3a4747287e3d3ebc939d741b5
theelgreco <stelios.kakoliris@yahoo.com>
2025-11-01 11:04:09 +0000

    fix: improve loader element handling and ensure opacity is set for hands

--- src/client/pages/loader/script.ts
+++ src/client/pages/loader/script.ts
@@ -1,14 +1,19 @@
 const loaders = document.querySelectorAll(".loader");
 
 loaders.forEach((loader) => {
-    const numHands = parseInt((loader as HTMLDivElement).dataset.numHands || "") || 20;
-    const animationDuration = parseInt((loader as HTMLDivElement).dataset.animationDuration || "") || 600;
+    const loaderElement = loader as HTMLDivElement;
+
+    const numHands = parseInt(loaderElement.dataset.numHands || "") || 20;
+    const animationDuration = parseInt(loaderElement.dataset.animationDuration || "") || 600;
 
     for (let i = 0; i < numHands; i++) {
         const hand = document.createElement("span");
         hand.style.transform = `rotate(${(360 / numHands) * i}deg)`;
         hand.style.animationDuration = `${animationDuration}ms`;
         hand.style.animationDelay = `${(animationDuration / numHands) * i}ms`;
+        hand.style.opacity = "0";
         loader.appendChild(hand);
     }
+
+    loaderElement.style.setProperty("--loader-height", loaderElement.style.height || `${loaderElement.parentElement?.offsetHeight || 0}px`);
 });
